cac565a959bf41dd4b4b51aadd754d862d5816cd
Rich Long <rich.long.ext@bbc.co.uk>
2015-03-11 10:18:52 +0000

    add sentiment

--- gettweets.js
+++ gettweets.js
@@ -7,13 +7,17 @@ var T = new Twit({
   , access_token_secret:  'VieqG0OJXAxprVtxbtKy81pXQcZYGCJ5Ypb3B1apTtYWT'
 })
 
-T.get('search/tweets', { q: 'bbc :)', count: 100 }, function(err, data, response) {
+var sentiment = require('sentiment');
+
+T.get('search/tweets', { q: 'topgear', count: 100 }, function(err, data, response) {
 
   var jsonString = JSON.stringify(data, null, 4);
   var jsonObj = JSON.parse(jsonString);
 	
     for ( var i = 0; i < jsonObj.statuses.length; i++ ) {
   		console.log(jsonObj.statuses[i].text);
+      var r1 = sentiment(jsonObj.statuses[i].text);
+      console.dir(r1);        // Score: -2, Comparative: -0.666 
 	}
 	console.log(jsonObj.statuses.length + " Results")
 
@@ -21,3 +25,4 @@ T.get('search/tweets', { q: 'bbc :)', count: 100 }, function(err, data, response
   	  console.log(err);
   	};
 })
+
